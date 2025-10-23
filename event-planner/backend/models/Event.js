85a0e6075ae60696039043a218e1428590c46805
Duwayne <duwayneblok01@gmail.com>
2025-10-23 11:12:48 +0200

    added guestSchema and updated eventSchema

--- event-planner/backend/models/Event.js
+++ event-planner/backend/models/Event.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
+import { type } from "os";
+
+const guestSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  invited: {type:Boolean, default:false},
+});
 
 const eventSchema = new mongoose.Schema({
   name: { type: String, required: true },
   date: { type: String, required: true },
   location: { type: String, required: true },
   description: { type: String },
-  password: { type: String, required: true },
+  eventPassword: { type: String  },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  guest: [guestSchema],
 });
 
 export default mongoose.model("Event", eventSchema);
