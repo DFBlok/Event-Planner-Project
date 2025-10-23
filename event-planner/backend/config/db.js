991d9e4d16e3852e90f2ce851698618d8bcf709b
Duwayne <duwayneblok01@gmail.com>
2025-10-23 10:59:54 +0200

    fixed backend connection issues

--- event-planner/backend/config/db.js
+++ event-planner/backend/config/db.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+
+//console.log("🧠 MONGO_URI:", process.env.MONGO_URI);
 
 export const connectDB = async () => {
   try {
